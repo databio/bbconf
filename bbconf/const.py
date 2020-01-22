63238973b70068b0ea55131871dd5b9f21e36e1d
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-01-22 16:27:47 -0500

    add raw bedfile key const

--- bbconf/const.py
+++ bbconf/const.py
@@ -5,3 +5,6 @@ Constant variables shared among packages that constitute bedbase project
 BED_INDEX = "bedstat_bedfiles"
 SEARCH_TERMS = ['cellType', 'cellTypeSubtype', 'antibody', 'mappingGenome',
                 'description', 'tissue', 'species', 'protocol', 'genome']
+RAW_BEDFILE_KEY = "raw_bedfile"
+
+__all__ = ["BED_INDEX", "SEARCH_TERMS", "RAW_BEDFILE_KEY"]
