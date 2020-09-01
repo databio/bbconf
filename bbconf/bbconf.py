051bd37a761ad87f899b401593f8826417acf963
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-09-01 15:40:10 -0400

    inserting enhancements, count rows

--- bbconf/bbconf.py
+++ bbconf/bbconf.py
@@ -1,8 +1,9 @@
 import psycopg2
-import psycopg2.extras
+from psycopg2.extras import DictCursor, Json
 
 from logging import getLogger
 from contextlib import contextmanager
+from collections import Mapping
 
 import yacman
 
@@ -54,7 +55,7 @@ class BedBaseConf(yacman.YacAttMap):
         return k in \
                [attr for attr in self.to_dict().keys() if attr.startswith("_")]
 
-    def establish_postgres_connection(self, host=None, suppress=False):
+    def establish_postgres_connection(self, suppress=False):
         """
         Establish PostgreSQL connection using the config data
 
@@ -151,15 +152,32 @@ class BedBaseConf(yacman.YacAttMap):
         :param str table_name: name of the table to insert the values to
         :param dict values: a mapping of pairs of table column names and
             respective values to bne inserted to the database
-        :return:
         """
         statement = f"INSERT INTO {table_name} ({','.join(values.keys())})" \
                     f" VALUES ({','.join(['%s'] * len(values))});"
-        values = tuple(values.values())
+        # convert mappings to JSON for postgres
+        values = tuple([Json(v) if isinstance(v, Mapping) else v
+                        for v in list(values.values())])
         with self.db_cursor as cur:
             _LOGGER.info(f"Inserting into DB:\n - statement: {statement}"
                          f"\n - values: {values}")
             cur.execute(statement, values)
+
+    def insert_bedfile_data(self, values):
+        """
+
+        :param dict values: a mapping of pairs of table column names and
+            respective values to bne inserted to the database
+        """
+        self._insert(table_name=BED_TABLE, values=values)
+
+    def insert_bedset_data(self, values):
+        """
+
+        :param dict values: a mapping of pairs of table column names and
+            respective values to bne inserted to the database
+        """
+        self._insert(table_name=BEDSET_TABLE, values=values)
 
     @property
     @contextmanager
@@ -173,216 +191,41 @@ class BedBaseConf(yacman.YacAttMap):
         try:
             self.establish_postgres_connection()
             connection = self[PG_CLIENT_KEY]
-            with connection as conn, conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            with connection as conn, conn.cursor(cursor_factory=DictCursor) as cur:
                 yield cur
         except:
             raise
         finally:
             self.close_postgres_connection()
 
-    # def _search_index(self, index_name, query, just_data=True, size=None,
-    #                   **kwargs):
-    #     """
-    #     Search selected Elasticsearch index with selected query
-    #
-    #     :param str index_name: name of the Elasticsearch index to search
-    #     :param dict query: query to search the DB against
-    #     :param bool just_data: whether just the hits should be returned
-    #     :param int size: number of hits to return, all are returned by default
-    #     :return dict | Iterable[dict] | NoneType: search results
-    #         or None if requested index does not exist
-    #     """
-    #     self.assert_connection()
-    #     if not self[ES_CLIENT_KEY].indices.exists(index_name):
-    #         _LOGGER.warning("'{}' index does not exist".format(index_name))
-    #         return
-    #     _LOGGER.debug("Searching index: {}\nQuery: {}".
-    #                   format(index_name, query))
-    #     query = {"query": query} if "query" not in query else query
-    #     size = size or self._count_docs(index=index_name)
-    #     search_results = self[ES_CLIENT_KEY].search(
-    #         index=index_name, body=query, size=size, **kwargs)
-    #     return [r["_source"] for r in search_results["hits"]["hits"]] \
-    #         if just_data else search_results
-    #
-    # def search_bedfiles(self, query, just_data=True, **kwargs):
-    #     """
-    #     Search selected Elasticsearch bedset index with selected query
-    #
-    #     :param dict query: query to search the DB against
-    #     :param bool just_data: whether just the hits should be returned
-    #     :return dict | Iterable[dict]: search results
-    #     """
-    #     return self._search_index(index_name=BED_INDEX, query=query,
-    #                               just_data=just_data, **kwargs)
-    #
-    # def search_bedsets(self, query, just_data=True, **kwargs):
-    #     """
-    #     Search selected Elasticsearch bedfiles index with selected query
-    #
-    #     :param dict query: query to search the DB against
-    #     :param bool just_data: whether just the hits should be returned
-    #     :return dict | Iterable[dict]: search results
-    #     """
-    #     return self._search_index(index_name=BEDSET_INDEX, query=query,
-    #                               just_data=just_data, **kwargs)
-    #
-    # def _insert_data(self, index, data, doc_id, force_update=False, **kwargs):
-    #     """
-    #     Insert document to an index in a Elasticsearch DB
-    #     or create it and the insert in case it does not exist.
-    #
-    #     Document ID argument is optional. If not provided, a random ID
-    #     will be assigned.
-    #     If provided the document will be inserted only if no documents with
-    #     this ID are present in the DB. However, the document overwriting
-    #     can be forced if needed.
-    #
-    #     :param str index: name of the index to insert the data into
-    #     :param str doc_id: unique identifier for the document
-    #     :param bool force_update: whether the pre-existing document
-    #      should be overwritten
-    #     :param dict data: data to insert
-    #     """
-    #     self.assert_connection()
-    #     if doc_id is None:
-    #         _LOGGER.info("Inserting document to index '{}' with an "
-    #                      "automatically-assigned ID".format(index))
-    #         self[ES_CLIENT_KEY].index(index=index, body=data, **kwargs)
-    #     else:
-    #         try:
-    #             self[ES_CLIENT_KEY].create(index=index, body=data, id=doc_id,
-    #                                        **kwargs)
-    #         except ConflictError:
-    #             msg_base = "Document '{}' already exists in index '{}'"\
-    #                 .format(doc_id, index)
-    #             if force_update:
-    #                 _LOGGER.info(msg_base + ". Forcing update")
-    #                 self[ES_CLIENT_KEY].index(index=index, body=data, id=doc_id,
-    #                                           **kwargs)
-    #             else:
-    #                 _LOGGER.error("Could not insert data. " + msg_base)
-    #                 raise
-    #
-    # def insert_bedfiles_data(self, data, doc_id=None, **kwargs):
-    #     """
-    #     Insert data to the bedfile index a Elasticsearch DB
-    #     or create it and the insert in case it does not exist.
-    #
-    #     Document ID argument is optional. If not provided, a random ID will
-    #     be assigned. If provided the document will be inserted only if no
-    #     documents with this ID are present in the DB. However, the document
-    #     overwriting can be forced if needed.
-    #
-    #     :param dict data: data to insert
-    #     :param str doc_id: unique identifier for the document, optional
-    #     """
-    #     self._insert_data(index=BED_INDEX, data=data, doc_id=doc_id, **kwargs)
-    #
-    # def insert_bedsets_data(self, data, doc_id=None, **kwargs):
-    #     """
-    #     Insert data to the bedset index in a Elasticsearch DB
-    #     or create it and the insert in case it does not exist.
-    #
-    #     Document ID argument is optional. If not provided, a random ID will
-    #     be assigned.
-    #     If provided the document will be inserted only if no documents with
-    #     this ID are present in the DB.
-    #     However, the document overwriting can be forced if needed.
-    #
-    #     :param dict data: data to insert
-    #     :param str doc_id: unique identifier for the document, optional
-    #     """
-    #     self._insert_data(index=BEDSET_INDEX, data=data, doc_id=doc_id,
-    #                       **kwargs)
-    #
-    # def _get_mapping(self, index, just_data=True, **kwargs):
-    #     """
-    #     Get mapping definitions for the selected index
-    #
-    #     :param str index: index to return the mappging for
-    #     :return dict: mapping definitions
-    #     """
-    #     self.assert_connection()
-    #     mapping = self[ES_CLIENT_KEY].indices.get_mapping(index, **kwargs)
-    #     return mapping[index]["mappings"]["properties"] \
-    #         if just_data else mapping
-    #
-    # def get_bedfiles_mapping(self, just_data=True, **kwargs):
-    #     """
-    #     Get mapping definitions for the bedfiles index
-    #
-    #     :return dict: bedfiles mapping definitions
-    #     """
-    #     return self._get_mapping(index=BED_INDEX, just_data=just_data, **kwargs)
-    #
-    # def get_bedsets_mapping(self, just_data=True, **kwargs):
-    #     """
-    #     Get mapping definitions for the bedsets index
-    #
-    #     :return dict: besets mapping definitions
-    #     """
-    #     return self._get_mapping(index=BEDSET_INDEX, just_data=just_data,
-    #                              **kwargs)
-    #
-    # def _get_doc(self, index, doc_id):
-    #     """
-    #     Get a document from an index by its ID
-    #
-    #     :param str index: name of the index to search
-    #     :param str doc_id: document ID to return
-    #     :return Mapping: matched document
-    #     """
-    #     return self[ES_CLIENT_KEY].get(index=index, id=doc_id)
-    #
-    # def get_bedfiles_doc(self, doc_id):
-    #     """
-    #     Get a document from bedfiles index by its ID
-    #
-    #     :param str doc_id: document ID to return
-    #     :return Mapping: matched document
-    #     """
-    #     return self._get_doc(index=BED_INDEX, doc_id=doc_id)
-    #
-    # def get_bedsets_doc(self, doc_id):
-    #     """
-    #     Get a document from bedsets index by its ID
-    #
-    #     :param str doc_id: document ID to return
-    #     :return Mapping: matched document
-    #     """
-    #     return self._get_doc(index=BEDSET_INDEX, doc_id=doc_id)
-    #
-    # def _count_docs(self, index):
-    #     """
-    #     Get the total number of the documents in a selected index
-    #
-    #     :param str index: index to count the documents for
-    #     :return int | None: number of documents
-    #     """
-    #     self.assert_connection()
-    #     if not self[ES_CLIENT_KEY].indices.exists(index=index):
-    #         _LOGGER.warning("'{}' index does not exist".format(index))
-    #         return None
-    #     return int(self[ES_CLIENT_KEY].cat.count(
-    #         index, params={"format": "json"})[0]['count'])
-    #
-    # def count_bedfiles_docs(self):
-    #     """
-    #     Get the total number of the documents in the bedfiles index
-    #
-    #     :return int: number of documents
-    #     """
-    #     return self._count_docs(index=BED_INDEX)
-    #
-    # def count_bedsets_docs(self):
-    #     """
-    #     Get the total number of the documents in the bedsets index
-    #
-    #     :return int: number of documents
-    #     """
-    #     return self._count_docs(index=BEDSET_INDEX)
-    #
+    def _count_rows(self, table_name):
+        """
+        Count rows in a selected table
+
+        :param str table_name: table to count rows for
+        :return int: numner of rows in the selected table
+        """
+        with self.db_cursor as cur:
+            cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+            return cur.fetchall()[0][0]
+
+    def count_bedfiles(self):
+        """
+        Count rows in the bedfiles table
+
+        :return int: numner of rows in the bedfiles table
+        """
+        return self._count_rows(table_name=BED_TABLE)
+
+    def count_bedsets(self):
+        """
+        Count rows in the bedsets table
+
+        :return int: numner of rows in the bedsets table
+        """
+        return self._count_rows(table_name=BEDSET_TABLE)
+
+
     # def _delete_index(self, index):
     #     """
     #     Delete selected index from Elasticsearch
